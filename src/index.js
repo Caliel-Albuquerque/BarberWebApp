aaf82b11da1001b123506c9dfce590e6ba8d6173
Arthur-Falcao <arthurmonteirofalcao1995@gmail.com>
2022-10-31 23:53:39 -0300

    Resgatando Dados de Clientes do BD

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 const express = require ('express')
 const app = express()
 
+//Importar model Cliente
+const Cliente = require('./models/Cliente')
 
+app.get('/clientes', (req, res) => {
+    Cliente.findAll().then((data) => {
+        if (data.length > 0) {
+            res.send(data)
+        } else {
+            res.send("Sem clientes cadastrados!")
+        }
+        
+    }).catch((err) => {
+        console.log(`Houve um problema: ${err}`)
+    })
+})
 
 
 
