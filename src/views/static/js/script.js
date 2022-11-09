3ce098598daa573213691324253485bd2a78282c
Caliel-Albuquerque <75955275+Caliel-Albuquerque@users.noreply.github.com>
2022-11-09 20:47:33 -0300

    Sidebar

---
+++ src/views/static/js/script.js
@@ -0,0 +1,11 @@
+//MENU-SIDEBAR 
+
+var buttonMenu = document.getElementById("contentMenu");  
+var sideBar = document.getElementById("contentSidebar");
+
+
+buttonMenu.addEventListener("click", () => {
+    sideBar.classList.toggle("ativar_menu")
+    
+})
+
