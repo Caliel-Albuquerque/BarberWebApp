be4877d23f307edf562ef58b3f6d545fef945305
Arthur-Falcao <arthurmonteirofalcao1995@gmail.com>
2022-12-10 12:56:56 -0300

    Usando bcrypt para encriptografar senha e sessao para salvar dados

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+//require('dotenv').config()
 const express = require ('express')
-const app = express()
+const bcrypt = require('bcrypt')
+//const jwt = require('jsonwebtoken')
 const hbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+
+const app = express()
 
 // Importar model Cliente
 const Cliente = require('./models/Cliente')
@@ -18,23 +23,83 @@ app.engine('hbs', hbs.engine({
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
-
+app.use(express.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
-/* ROTAS */
+//Confuguração das Sessions
+app.use(session({
+    secret:'123blablabla',
+    resave: false,
+    saveUninitialized: true
+}))
 
-/* INICIO GET INDEX */
+/* ROTAS */
 app.get('/', (req, res) => {
-    res.render('login')
+    res.render('login', {error: false})
 })
 
-/* INICIO GET NOVO USUARIO */
+// Login de Usuário
+app.post('/', async(req, res) => {
+    const {email, senha} = req.body
+
+    //Array que vai conter os erros
+    let erros = []
+
+    /* Verificar se está vazio ou não definido */
+    if (email == '' || typeof email == undefined || email == null) {
+        erros.push({mensagem: "Campo email não pode ser vazio!"})
+    }
+    
+    /* Verificar se está vazio ou não definido */
+    if (senha == '' || typeof senha == undefined || senha == null) {
+        erros.push({mensagem: "Campo senha não pode ser vazio!"})
+    }
+
+    // Checar se usuário existe
+    const user = await Usuario.findByPk(email)
+    
+    const userExists = user instanceof Usuario
+    
+    if (!userExists) {
+        erros.push({mensagem:"Email Inválido."})
+    } else {
+        const userData = user.toJSON()
+        // Checar se senha combina
+        const checkPassword = await bcrypt.compare(senha, userData.senha)
+
+        if (!checkPassword) {
+            erros.push({mensagem: "Senha Inválida."})
+        }
+    }
+
+    if (erros.length > 0) {
+        return res.render('login', {error: true, problemas: erros})
+    } else {
+        const userData = user.toJSON()
+        req.session.userName = userData.nome
+        let userName = req.session.userName
+
+        return res.redirect(`/in/${userName}`)
+    }
+
+    /*const secret = process.env.SECRET
+    const token = jwt.sign(
+        {
+            id: user.nome
+        },
+        secret
+    )*/
+
+    
+})
+
+// Exibir página de cadastro de usuário
 app.get('/novoUsuario', (req, res) => {
     res.render('novoUsuario')
 })
-/* FIM GET NOVO USUARIO */
 
-app.post('/cadUsuario', (req, res) => {
+// Cadastro de Usuário
+app.post('/cadUsuario', async(req, res) => {
     let nome = req.body.nome
     let tel = req.body.telefone
     let email = req.body.email
@@ -75,56 +140,90 @@ app.post('/cadUsuario', (req, res) => {
         erros.push({mensagem:"Campo email Inválido!"})
     }
 
-    if(erros.length > 0) {
-        return res.redirect('/login')
+    const user = await Usuario.findByPk(email)
+    const userExists = user instanceof Usuario
+
+    if (userExists) {
+        erros.push({mensagem:"Por favor, utilize outro email."})
     }
 
+    if(erros.length > 0) {
+        return res.redirect('/novoUsuario')
+    }
+
+    // Criar senha
+    const salt = await bcrypt.genSalt(12)
+    const senhaHash = await bcrypt.hash(senha, salt)
+
     //Sucesso (Nenhum Erro) - Salvar no BD
-    Usuario.create({
+    await Usuario.create({
         nome: nome,
         telefone: tel,
         email: email.toLowerCase(),
-        senha: senha
+        senha: senhaHash
     }).then(function() {
-        console.log('Cadastrado com sucesso!') 
-        return res.redirect('/home')
+        console.log('Cadastrado com sucesso!')
+        res.redirect('/')
     }).catch(function(err) {
         console.log(`Ops, houve um erro: ${err}`)
         res.redirect('/novoUsuario')
     })
 })
 
-app.get('/home', (req, res) => {
-    res.render('index')
+// Middleware
+/*
+function checkToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if (!token) {
+        return res.status(401).json({msg: "Acesso negado."})
+    }
+
+    try {
+        const secret = process.env.SECRET
+
+        jwt.verify(token, secret)
+
+        // Finalizou o middleware
+        next()
+
+    } catch(error) {
+        res.status(400).json({msg: "Token Inválido."})
+    }
+}*/
+
+// Exibir página inicial do app
+app.get('/in/:nome', (req, res) => {
+    const nome = req.params.nome
+    res.render('index', {nomeUsuario: nome})
 })
-/* FIM GET INDEX */
 
-
-/* INICIO GET SERVICOS */
-app.get('/servicos', async (req, res) => {
+// Exibir/listar serviços cadastrados
+app.get('/servicos', async(req, res) => {
+    const nomeUsuario = req.session.userName
     const { count, rows } = await Servico.findAndCountAll()
-    let numServicos = count
 
     await Servico.findAll()
     .then((servicos) => {
         res.render('servicos', { 
             servicos: servicos.map((servicos) => servicos.toJSON() ),
-            numServicos: numServicos
+            numServicos: count,
+            nomeUsuario: nomeUsuario
          })
+         console.log(nome)
     })
     .catch((err) => console.log(err))
 })
-/* FIM GET SERVICOS */
 
-
-/* INICIO GET NOVO SERVICO */
 app.get('/novoservico', (req, res) => {
+    //const nome = req.params.nome
     res.render('novoServico')
 })
-/* FIM GET NOVO SERVICO */
 
-/* INICIO POST SERVICOS */
-app.post('/cadServico', (req, res) => {
+
+// Cadastrar novo serviço
+app.post('/cadServico', async(req, res) => {
     //Valores vindos do formulário
     let nome = req.body.nome
     let valor = req.body.valor
@@ -154,42 +253,40 @@ app.post('/cadServico', (req, res) => {
     }
 
     //Sucesso (Nenhum Erro) - Salvar no BD
-    Servico.create({
+    await Servico.create({
         nome: nome,
         valor: valor
     }).then(function() {
         console.log('Cadastrado com sucesso!') 
-        return res.redirect('/servicos')
+        return res.redirect(`/servicos`)
     }).catch(function(err) {
         console.log(`Ops, houve um erro: ${err}`)
     })
 })
-/* FIM POST SERVICOS */
 
-// INICIO GET EDITAR-SERVICO
-app.post('/editarServico', (req, res) => {
+// Exibir serviço a ser editado
+app.post('/editarServico', async(req, res) => {
     let idServico = req.body.idServico
-    Servico.findByPk(idServico).then((dados) => {
+    console.log(idServico)
+    await Servico.findByPk(idServico).then((dados) => {
+        console.log(dados)
         return res.render('editarServico', {
             error: false, 
             idServico: dados.idServico, 
             nome: dados.nome, 
-            valor: dados.valor
+            valor: dados.valor,
         })
     }).catch((err) => {
-        console.log(err)
-        return res.render('editarServico', {
+        //console.log(err)
+        return res.render(`editarServico`, {
             error: true, 
-            problema: 'Não é possível editar esse registro!'
+            problema: 'Não é possível editar esse registro!',
         })
     })
 })
-// FIM GET EDITAR-SERVICO
 
-// INICIO PUT EDITAR-SERVICO
-app.post('/updateServico', (req, res) => {
-    let body = req.body
-    console.log(body)
+// Editar serviço
+app.post('/updateServico', async(req, res) => {
     //Valores vindos do formulário
     let nome = req.body.nome
     let valor = req.body.valor
@@ -218,7 +315,7 @@ app.post('/updateServico', (req, res) => {
         erros.push({mensagem: "Campo valor não pode ser vazio!"})
     }
 
-    Servico.update({
+    await Servico.update({
         nome: nome,
         valor: valor
     },
@@ -228,29 +325,29 @@ app.post('/updateServico', (req, res) => {
         }
     }).then((resultado) => {
         console.log(resultado)
-        return res.redirect('/servicos')
+        return res.redirect(`/servicos`)
     }).catch((err) => {
         console.log(err)
     })
 })
-// FINAL PUT EDITAR-SERVICO
 
-/* INICIO DELETE SERVICO */
-app.post('/deletarServico', (req, res) => {
+// Deletar serviço
+app.post('/deletarServico', async(req, res) => {
     let idServico = req.body.idServico
-    Servico.destroy({
+    await Servico.destroy({
         where:{
             idServico: idServico
         }
     }).then(() => {
-        return res.redirect('/servicos')
+        return res.redirect(`/servicos`)
     }).catch((err) => {
         console.log(err)
     })
 })
 
-/* INICIO GET CLIENTES */
+// Exibir/listar clientes cadastrados
 app.get('/clientes', async (req, res) => {
+    let nomeUsuario = req.session.userName
     const { count, rows } = await Cliente.findAndCountAll()
     let numClientes = count
 
@@ -258,17 +355,20 @@ app.get('/clientes', async (req, res) => {
     .then((cliente) => {
         res.render('clientes', { 
             cliente: cliente.map((clientes) => clientes.toJSON() ),
-            numCliente: numClientes
+            numCliente: numClientes,
+            nomeUsuario: nomeUsuario
          })
     })
     .catch((err) => console.log(err))
 })
-/* INICIO GET NOVOCLIENTE */
-app.get("/novocliente", (req, res) => {
+
+// Exibir página de cadastro de novo cliente
+app.get("/novoCliente", (req, res) => {
     res.render("novoCliente")
 })
-/* INICIO POST CLIENTES */
-app.post('/cadCliente', (req, res) => {
+
+// Cadastro de cliente
+app.post('/cadCliente', async(req, res) => {
     //Valores vindos do formulário
     let nome = req.body.nomeCliente
     let celularCliente = req.body.celularCliente
@@ -316,21 +416,20 @@ app.post('/cadCliente', (req, res) => {
     
 
     //Sucesso (Nenhum Erro) - Salvar no BD
-    Cliente.create({
+    await Cliente.create({
         nome: nome,
         telefone: celularCliente,
         email: emailCliente,
         data_nasc: aniversarioCliente 
     }).then(function() {
         console.log('Cadastrado com sucesso!') 
-        return res.redirect('/clientes')
+        return res.redirect(`/clientes`)
     }).catch(function(err) {
         console.log(`Ops, houve um erro: ${err}`)
     })
 })
-/* FIM POST CLIENTES */
 
-/* INICIO DELETE CLIENTE */
+// Deletar cliente
 app.post('/deletarCliente', (req, res) => {
     let idCliente = req.body.idCliente
     Cliente.destroy({
@@ -338,16 +437,15 @@ app.post('/deletarCliente', (req, res) => {
             idCliente: idCliente
         }
     }).then(() => {
-        return res.redirect('/clientes')
+        return res.redirect(`/clientes`)
     }).catch((err) => {
         console.log(err)
     })
 })
 
-/* INICIO GET ATENDIMENTO */ 
+// Exibir atendimentos cadastrados
 app.get("/atendimento", async (req, res) => {
     
-
     await Servico.findAll()
     .then((servicos) => {
         res.render('venda', { 
