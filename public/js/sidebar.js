84c20b866a01c5326d8361d45de21763bb5c0d6c
Caliel-Albuquerque <75955275+Caliel-Albuquerque@users.noreply.github.com>
2022-12-10 20:10:11 -0300

    mask Input

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,6 +1,6 @@
 //MENU-SIDEBAR 
 
-var buttonMenu = document.getElementById("contentMenu");  
+var buttonMenu = document.getElementById("contentMenu");
 var sideBar = document.getElementById("contentSidebar");
 var iconMenu = document.getElementById("icon_menu");
 
@@ -11,16 +11,19 @@ var closeSidebarServico = document.getElementById("containerServico");
 buttonMenu.addEventListener("click", () => {
     sideBar.classList.toggle("ativar_menu")
 
-    
 
-    
+
+
 })
 
-closeSidebar.addEventListener("click", () =>{
+closeSidebar.addEventListener("click", () => {
     sideBar.classList.remove("ativar_menu")
 })
 
-closeSidebarServico.addEventListener("click", () =>{
+closeSidebarServico.addEventListener("click", () => {
     sideBar.classList.remove("ativar_menu")
 })
+
+
+
 
